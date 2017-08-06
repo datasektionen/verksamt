db1893e06eb75fe50cf250f843ab621f7ed573ea
viklu <viklu@kth.se>
2017-08-06 06:27:19 +0200

    Add models to represent the basics of the operational plan. Close #1

--- verksamhetsplan/admin.py
+++ verksamhetsplan/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from verksamhetsplan.models import *
 
-# Register your models here.
+admin.site.register(Year)
+admin.site.register(OperationalArea)
+admin.site.register(LongTermGoal)
+admin.site.register(Goal)
+admin.site.register(Status)
--- verksamhetsplan/models.py
+++ verksamhetsplan/models.py
@@ -1,7 +1,44 @@
 from django.db import models
 
-# Create your models here.
+
+class Year(models.Model):
+    name = models.TextField()
+
+    def __unicode__(self):
+        return str(self.name)
 
 
-class Verksamhetsplan(models.Model):
-    year =
+class OperationalArea(models.Model):
+    name = models.TextField()
+
+    def __unicode__(self):
+        return str(self.name)
+
+
+class LongTermGoal(models.Model):
+    goal = models.TextField()
+    description = models.TextField()
+    operational_area = models.ForeignKey(OperationalArea, on_delete=models.CASCADE)
+
+    def __unicode__(self):
+        return str(self.goal)
+
+    def first_year(self):
+        return Year.objects.filter(goal__in=self.goal_set).first()
+
+
+class Goal(models.Model):
+    year = models.ForeignKey(Year, on_delete=models.CASCADE)
+    long_term_goal = models.ForeignKey(LongTermGoal, on_delete=models.CASCADE)
+    goal = models.TextField()
+    description = models.TextField()
+
+    def __unicode__(self):
+        return str(self.goal)
+
+
+class Status(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __unicode__(self):
+        return str(self.name)
