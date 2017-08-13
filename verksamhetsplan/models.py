cc8fd61cf0ddf0782c9fafd316a3abb43b865063
Alexander Viklund <viklu@kth.se>
2017-08-13 18:49:58 +0200

    Add str methods to models

--- verksamhetsplan/models.py
+++ verksamhetsplan/models.py
@@ -6,23 +6,32 @@ class OperationalPlan(models.Model):
     description = models.TextField()
 
     def __unicode__(self):
-        return str(self.year)
+        return self.year
+
+    def __str__(self):
+        return self.year
 
 
 class OperationalArea(models.Model):
     name = models.TextField()
 
     def __unicode__(self):
-        return str(self.name)
+        return self.name
+
+    def __str__(self):
+        return self.name
 
 
 class LongTermGoal(models.Model):
     goaltext = models.TextField()
-    description = models.TextField()
+    description = models.TextField(default="", blank=True)
     operational_area = models.ForeignKey(OperationalArea, on_delete=models.CASCADE)
 
     def __unicode__(self):
-        return str(self.goaltext)
+        return self.goaltext
+
+    def __str__(self):
+        return self.goaltext
 
     def first_year(self):
         return OperationalPlan.objects.filter(goal__in=self.goal_set).first()
@@ -32,14 +41,20 @@ class Goal(models.Model):
     year = models.ForeignKey(OperationalPlan, on_delete=models.CASCADE)
     long_term_goal = models.ForeignKey(LongTermGoal, on_delete=models.CASCADE)
     goal = models.TextField()
-    description = models.TextField()
+    description = models.TextField(default="", blank=True)
 
     def __unicode__(self):
-        return str(self.goal)
+        return self.goal
+
+    def __str__(self):
+        return self.goal
 
 
 class Status(models.Model):
     name = models.CharField(max_length=10)
 
     def __unicode__(self):
-        return str(self.name)
+        return self.name
+
+    def __str__(self):
+        return self.name
