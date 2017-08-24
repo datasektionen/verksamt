0e183efb8a231237bec7813b6ce6633dc46b6046
Alexander Viklund <viklu@kth.se>
2017-08-24 03:20:44 +0200

    Add really ugly way of displaying a goal

--- verksamhetsplan/urls.py
+++ verksamhetsplan/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('^$', views.index)
+    url('^$', views.index),
+    url('^goal/(?P<pk>\d+)$', views.goal_by_id),
 ]
--- verksamhetsplan/views.py
+++ verksamhetsplan/views.py
@@ -1,5 +1,18 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
 from django.shortcuts import render
+
+from . import models
 
 
 def index(request):
     return render(request, "verksamhetsplan/main.html")
+
+
+def goal_by_id(request, pk):
+    try:
+        goal = models.Goal.objects.get(pk=int(pk))
+        if request.method == 'GET':
+            return render(request, "verksamhetsplan/goal.html", {'goal': goal})
+    except ObjectDoesNotExist:
+        raise Http404("Målet finns inte")
