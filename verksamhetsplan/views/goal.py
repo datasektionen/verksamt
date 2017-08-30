98504edb77c2e1928b5b37799f2bfe481dd4d6e6
Alexander Viklund <viklu@kth.se>
2017-08-30 07:42:25 +0200

    Add views for creating comments

--- verksamhetsplan/views/goal.py
+++ verksamhetsplan/views/goal.py
@@ -1,6 +1,7 @@
 from django.core.exceptions import ObjectDoesNotExist
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from verksamhetsplan import models
 
@@ -12,3 +13,14 @@ def goal_by_id(request, pk):
             return render(request, "verksamhetsplan/goal.html", {'goal': goal})
     except ObjectDoesNotExist:
         raise Http404("Målet finns inte")
+
+
+def create_comment(request, pk):
+    try:
+        goal = models.Goal.objects.get(pk=int(pk))
+    except ObjectDoesNotExist:
+        raise Http404("Målet finns inte")
+    if request.method == 'POST':
+        comment = models.Comment(goal=goal, author=request.user, content=request.POST['content'])
+        comment.save()
+        return HttpResponseRedirect(reverse('vp-goal', args=[goal.id]))
--- verksamhetsplan/views/long_term_goal.py
+++ verksamhetsplan/views/long_term_goal.py
@@ -1,6 +1,7 @@
 from django.core.exceptions import ObjectDoesNotExist
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from verksamhetsplan import models
 
@@ -15,3 +16,14 @@ def long_term_goal_by_id(request, pk):
             })
     except ObjectDoesNotExist:
         raise Http404("Målet finns inte")
+
+
+def create_comment(request, pk):
+    try:
+        goal = models.LongTermGoal.objects.get(pk=int(pk))
+    except ObjectDoesNotExist:
+        raise Http404("Målet finns inte")
+    if request.method == 'POST':
+        comment = models.Comment(long_term_goal=goal, author=request.user, content=request.POST['content'])
+        comment.save()
+        return HttpResponseRedirect(reverse('vp-long_goal', args=[goal.id]))
