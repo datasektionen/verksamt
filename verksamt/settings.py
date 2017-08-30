7ba256be79566d79ab7a674ad740c9053a298be7
Alexander Viklund <viklu@kth.se>
2017-08-30 18:49:10 +0200

    Add things for dokku-deployment

--- verksamt/settings.py
+++ verksamt/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'very_secret_secret_that_is_totaly_random')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DEBUG', False))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
